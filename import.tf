4cb1fa3e705c14a64e379d174f6f6cb251985e05
rkswisscom”git config --global user.email “reto.kernen@swisscom.com <Reto.Kernen@swisscom.com>
2025-03-11 15:57:13 +0100

    import test1

--- import.tf
+++ import.tf
@@ -1,15 +1,13 @@
-/*
-
 //Import guardrails as bloc
 locals {
   //OU ARN
-  target_identifier = "arn:aws:organizations::268702346055:ou/o-9ao1kn1kyw/ou-nmu5-5l01e2ro"
+  target_identifier = "arn:aws:organizations::200223571282:ou/o-x9clds5k02/ou-oqgn-17e9npip"
 
   //ARN of Guardrail -> https://docs.aws.amazon.com/controltower/latest/controlreference/control-metadata-tables.html
   guardrails = {
     guardrail_1 = {
-      control_identifier = "arn:aws:controltower:eu-central-1::control/AWS-GR_CONFIG_AGGREGATION_AUTHORIZATION_POLICY"
-    }
+      control_identifier = "arn:aws:controltower:eu-west-1::control/UAUKDTHXFEXN"
+    }/*
     guardrail_2 = {
       control_identifier = "arn:aws:controltower:eu-central-1::control/AWS-GR_CLOUDTRAIL_CHANGE_PROHIBITED"
     }
@@ -18,12 +16,10 @@ locals {
     }
     guardrail_4 = {
       control_identifier = "arn:aws:controltower:eu-central-1::control/AWS-GR_IAM_USER_MFA_ENABLED"
-    }
+    }*/
   }
 
 }
-
-
 
 resource "aws_controltower_control" "guardrails" {
   for_each           = local.guardrails
@@ -36,10 +32,6 @@ import {
   id       = "${local.target_identifier},${each.value.control_identifier}"
   to       = aws_controltower_control.guardrails[each.key]
 }
-
-
-
-
 
 /*
 //This code works to import one guardrail at a time
