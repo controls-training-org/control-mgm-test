f0840ec7abd5baee86c52c032009b48591c12f8d
rkswisscom”git config --global user.email “reto.kernen@swisscom.com <Reto.Kernen@swisscom.com>
2025-03-18 13:56:00 +0100

    test flag

--- import.tf
+++ import.tf
@@ -8,27 +8,12 @@ locals {
   guardrails = {
     guardrail_test = {
       control_identifier = "arn:aws:controlcatalog:::control/5mhjhod4ky44haldvja2v4x3a" # CT.APIGATEWAY.PR.1
-      prevent_destroy   = false # This guardrail won't be protected for Terraform Destroy
+      prevent_destroy   = false # This guardrail won't be protected (non-mandatory guardrail)
     }
     guardrail_2 = {
       control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_LAMBDA_CHANGE_PROHIBITED" # AWS-GR_LAMBDA_CHANGE_PROHIBITED
-      prevent_destroy   = true  # This guardrail will be protected for Terraform Destroy
-    }/*
-    guardrail_5 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK" # AWS-GR_EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK
+      prevent_destroy   = true  # This guardrail will be protected for Terraform Destroy (mandatory guardrail)
     }
-    guardrail_6 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_RESTRICT_ROOT_USER_ACCESS_KEYS" # AWS-GR_RESTRICT_ROOT_USER_ACCESS_KEYS
-    }
-    guardrail_7 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/BRGLCIPEZUAS" # CT.LAMBDA.PV.1
-    }
-    guardrail_8 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/UWYKAUHSRUKZ" # CT.DOCUMENTDB.PR.1
-    }
-    guardrail_9 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_RESTRICT_ROOT_USER" # AWS-GR_RESTRICT_ROOT_USER
-    }*/
   }
 
 }
@@ -37,6 +22,10 @@ resource "aws_controltower_control" "guardrails" {
   for_each           = local.guardrails
   control_identifier = each.value.control_identifier
   target_identifier  = local.target_identifier
+
+  lifecycle {
+    prevent_destroy = lookup(each.value, "prevent_destroy", false)
+  }
 }
 
 import {
