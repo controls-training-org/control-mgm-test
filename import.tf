2d8d1a483fd21df2dbbb3fa8b1a1047138491d4d
rkswisscom”git config --global user.email “reto.kernen@swisscom.com <Reto.Kernen@swisscom.com>
2025-03-18 14:01:06 +0100

    test flag

--- import.tf
+++ import.tf
@@ -5,33 +5,55 @@ locals {
   target_identifier = "arn:aws:organizations::200223571282:ou/o-x9clds5k02/ou-oqgn-17e9npip"
 
   //ARN of Guardrail -> https://docs.aws.amazon.com/controltower/latest/controlreference/control-metadata-tables.html
-  guardrails = {
+  # Guardrails which can be destroyed
+  guardrails_destroyable = {
     guardrail_test = {
       control_identifier = "arn:aws:controlcatalog:::control/5mhjhod4ky44haldvja2v4x3a" # CT.APIGATEWAY.PR.1
-      prevent_destroy   = false # This guardrail won't be protected (non-mandatory guardrail)
-    }
-    guardrail_2 = {
-      control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_LAMBDA_CHANGE_PROHIBITED" # AWS-GR_LAMBDA_CHANGE_PROHIBITED
-      prevent_destroy   = true  # This guardrail will be protected for Terraform Destroy (mandatory guardrail)
     }
   }
 
+  # Guardrails which cannot be destroyed (Mandatory guardrails)
+  guardrails_protected = {
+    guardrail_2 = {
+      control_identifier = "arn:aws:controltower:eu-west-1::control/AWS-GR_LAMBDA_CHANGE_PROHIBITED" # AWS-GR_LAMBDA_CHANGE_PROHIBITED
+    }
+  }
 }
 
-resource "aws_controltower_control" "guardrails" {
-  for_each           = local.guardrails
+# Resource for destroyable guardrails
+resource "aws_controltower_control" "destroyable_guardrails" {
+  for_each           = local.guardrails_destroyable
   control_identifier = each.value.control_identifier
   target_identifier  = local.target_identifier
 
   lifecycle {
-    prevent_destroy = lookup(each.value, "prevent_destroy", false)
+    prevent_destroy = false
   }
 }
 
+# Resource for protected guardrails
+resource "aws_controltower_control" "protected_guardrails" {
+  for_each           = local.guardrails_protected
+  control_identifier = each.value.control_identifier
+  target_identifier  = local.target_identifier
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Import blocks for destroyable guardrails
 import {
-  for_each = local.guardrails
+  for_each = local.guardrails_destroyable
   id       = "${local.target_identifier},${each.value.control_identifier}"
-  to       = aws_controltower_control.guardrails[each.key]
+  to       = aws_controltower_control.destroyable_guardrails[each.key]
+}
+
+# Import blocks for protected guardrails
+import {
+  for_each = local.guardrails_protected
+  id       = "${local.target_identifier},${each.value.control_identifier}"
+  to       = aws_controltower_control.protected_guardrails[each.key]
 }
 
 /*
